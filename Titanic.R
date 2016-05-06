170e84a62759099c8e86890dd2140ee36dddbb88
Pascal van de Wijdeven <pvandewijdeven@magion.nl>
2016-05-06 11:54:30 +0200

    Small test update

--- Titanic.R
+++ Titanic.R
@@ -13,3 +13,4 @@ test$Survived <- rep(0,418)
 submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
 write.csv(submit, file = "theyallperish.csv", row.names = FALSE)
 View(submit)
+# now commit this small change
