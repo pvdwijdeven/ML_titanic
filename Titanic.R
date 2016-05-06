a0ae85ded37763331c75b14d1342ce4521eaeb84
Pascal van de Wijdeven <pvandewijdeven@magion.nl>
2016-05-06 11:49:26 +0200

    Initial setup

---
+++ Titanic.R
@@ -0,0 +1,15 @@
+# set path
+# can I use github here?
+setwd("H:/R/Titanic")
+# import training and test data
+# strings are read as Factors, else use:
+# train <- read.csv("H:/R/Titanic/train.csv",stringsAsFactors=FALSE
+train <- read.csv("H:/R/Titanic/train.csv")
+View(train)
+test <- read.csv("H:/R/Titanic/test.csv")
+View(test)
+# asume nobody survived in test-set
+test$Survived <- rep(0,418)
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "theyallperish.csv", row.names = FALSE)
+View(submit)
